3b488d37d1e71882412fecf84b6a1f304e1334a7
Parker <parkermatheson@gmail.com>
2018-01-31 15:34:55 -0700

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    ParkMathViewTool::Renderer.copyright 'Parker Matheson', 'All rights reserved'
+  end
 end
