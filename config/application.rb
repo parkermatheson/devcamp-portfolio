f35ba782a7e16db15cebc0aed920a70347b1df91
Parker <parkermatheson@gmail.com>
2018-02-16 11:34:35 -0700

    updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,15 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -28,5 +28,6 @@ module DevcampPortfolio
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV['SECRET_KEY_BASE']
   end
 end
